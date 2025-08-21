51f17f6ab0005b2f2a084c8572fff6e547f99dbf
md-zeon <zeon.cse@gmail.com>
2025-08-21 14:13:21 +0000

    Added tailwind theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-600 font-bold">Welcome to Next js</h1>
+      <h1 className="text-3xl text-light-500 font-bold">Welcome to Next js</h1>
     </div>
   );
 };
