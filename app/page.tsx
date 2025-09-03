2bcd84029b2091a71efbdea0b295bd3f1ab61f8f
md-zeon <zeon.cse@gmail.com>
2025-09-03 12:35:58 +0000

    Added custom fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
     <div>
+      <h1 className="h1-bold font-inter">Welcome to Next js</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next js</h1>
       <h1 className="h1-bold">Welcome to Next js</h1>
     </div>
   );
