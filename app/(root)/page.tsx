472702548a9059258f5bd4ae753e1c261d553bff
md-zeon <zeon.cse@gmail.com>
2025-09-04 14:22:46 +0000

    Implemented Github Sign in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-const Home = () => {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <div>
       <h1 className="h1-bold font-inter">Welcome to Next js</h1>
       <h1 className="h1-bold font-space-grotesk">Welcome to Next js</h1>
       <h1 className="h1-bold">Welcome to Next js</h1>
+      <form
+        className="px-10"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
     </div>
   );
 };
