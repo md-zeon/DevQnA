1665f2feaf691873eda85c5173597f1e8a468dde
md-zeon <zeon.cse@gmail.com>
2025-09-06 03:54:15 +0000

    Structured Home Layout more

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,28 @@
-import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
   return (
-    <div>
-      <h1 className="h1-bold font-inter">Welcome to Next js</h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome to Next js</h1>
-      <h1 className="h1-bold">Welcome to Next js</h1>
-      {session && (
-        <form
-          className="px-10"
-          action={async () => {
-            "use server";
-            await signOut({ redirectTo: ROUTES.SIGN_IN });
-          }}
+    <>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
         >
-          <Button type="submit">Log out</Button>
-        </form>
-      )}
-    </div>
+          <Link href={ROUTES.ASK_QUESTION}>Ask Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search</section>
+      HomeFIlter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question card 1</p>
+        <p>Question card 2</p>
+        <p>Question card 3</p>
+        <p>Question card 4</p>
+      </div>
+    </>
   );
 };
 
