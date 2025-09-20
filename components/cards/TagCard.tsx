a1fc3e3013f41cf14618c2dc1038dfe334bfca53
md-zeon <zeon.cse@gmail.com>
2025-09-20 15:24:27 +0000

    Implemented Tag input

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -2,6 +2,7 @@ import ROUTES from "@/constants/routes";
 import Link from "next/link";
 import { Badge } from "../ui/badge";
 import { getDevIconClassName } from "@/lib/utils";
+import Image from "next/image";
 
 interface Props {
   _id: string;
@@ -9,23 +10,68 @@ interface Props {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({
+  _id,
+  name,
+  questions,
+  showCount,
+  compact,
+  remove,
+  isButton,
+  handleRemove,
+}: Props) => {
   const iconClass = getDevIconClassName(name);
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+
+  const Content = (
+    <>
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
       </Badge>
+
+      {remove && (
+        <Image
+          src="/icons/close.svg"
+          width={12}
+          height={12}
+          alt="Close Icon"
+          className="cursor-pointer object-contain invert-0 dark:invert"
+          onClick={handleRemove}
+        />
+      )}
+
       {showCount && (
         <p className="small-medium text-dark500_light700">{questions}</p>
       )}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button type="button" className="flex justify-between gap-2">
+        {Content}
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+          <div className="flex-center space-x-2">
+            <i className={`${iconClass} text-sm`}></i>
+            <span>{name}</span>
+          </div>
+        </Badge>
+        {showCount && (
+          <p className="small-medium text-dark500_light700">{questions}</p>
+        )}
+      </Link>
+    );
+  }
 };
 
 export default TagCard;
