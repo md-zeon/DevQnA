ab4bfde24ac2e35618f8f1a982c6506157434bb0
md-zeon <zeon.cse@gmail.com>
2025-10-04 07:31:52 +0600

    Implement getAllUsers server action

--- types/global.d.ts
+++ types/global.d.ts
@@ -63,3 +63,15 @@ interface Answer {
   downvotes: number;
   createdAt: Date;
 }
+
+interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
